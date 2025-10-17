a1c47dd4768149c5e18b338808c5cf0fe5950435
João Pedro Lopes <joaopalopes24@gmail.com>
2025-10-17 10:43:25 -0300

    feat(middleware): adding global middleware to use in routes with token

---
+++ src/Middleware/EnsureValidPersonalToken.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace PersonalTokens\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use PersonalTokens\Actions\ValidPersonalToken;
+
+class EnsureValidPersonalToken
+{
+    /**
+     * Handle an incoming request.
+     */
+    public function handle(Request $request, Closure $next, mixed $type = null)
+    {
+        $token = $request->input('token');
+
+        if (! ValidPersonalToken::handle($token, $type)) {
+            abort(401, trans('Invalid or expired personal token.'));
+        }
+
+        return $next($request);
+    }
+}
